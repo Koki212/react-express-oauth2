10849533648130bb3b9c580d768be288e187a810
Andreas Kasch <andreas.kasch@gmail.com>
2023-10-25 14:21:34 +0200

    connect db, prepare .env, jwt, bcrypt

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,36 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import { MongoClient } from 'mongodb';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 
-const app = express();
-const port = 3000;
+import 'dotenv/config';
 
-app.use(cors());
-app.use(bodyParser.json());
+// jwt: https://github.com/auth0/node-jsonwebtoken
+// bcrypt: https://github.com/kelektiv/node.bcrypt.js
 
-app.get('/api', (req, res) => {
-  res.send('Hello API!');
-});
+try {
+    const client = new MongoClient(process.env.MONGODB_CONNECTION_STRING);
+    const db = client.db('oauth2_demo');
+    const colUsers = db.collection('users');
 
-app.use(express.static('./frontend/dist'));
+    const app = express();
+    const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+    app.use(cors());
+    app.use(bodyParser.json());
+
+    app.get('/api', (req, res) => {
+    res.send('Hello API!');
+    });
+
+    app.use(express.static('./frontend/dist'));
+
+    app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+    });
+
+} catch(e) {
+    console.error(e);
+}
